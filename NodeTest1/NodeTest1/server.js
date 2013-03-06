3079a027aa1d72e731a11504d5edfb94c88f5aa3
shoobah <shoobah@gmail.com>
2013-03-06 23:46:48 +0100

    Testar JSON

---
+++ NodeTest1/NodeTest1/server.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="./nodelib/node.js"/>
+
+var url = require('url'),
+    path = require('path'),
+    http = require('http')
+        .createServer(function (request, response) {
+            var dest = url.parse(request.url).pathname;
+            console.log('url [' + dest + '] requested');
+            response.writeHead(200, { 'Content-Type': 'text/html' });
+            var input = JSON.stringify(require('url').parse(request.url,true).query);
+            response.end(input);
+        }).listen(8080);
